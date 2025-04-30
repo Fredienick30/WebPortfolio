2f594d7992e64e57b8da63ac3f548dacba7449dd
Nickie _ <Josonfredienick30@gmail.com>
2025-04-30 22:34:58 +0800

    Initial commit - Web Portfolio

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /* Multiple Text */
 const typed = new Typed('.multiple-text', {
-    strings: ['IT Student', 'Gamer', 'Newbie Developer'],
+    strings: ['IT Student', 'Gamer', 'Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
